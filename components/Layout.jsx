785e937896ae411e62c864c582d3f8af66b7e5c6
mozumasu <ouri1229@gmail.com>
2023-10-18 11:45:29 +0900

    ホームへ戻るボタンを追加

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from './layout.module.css';
 import utilStyles from '../src/styles/utils.module.css';
+import Link from 'next/link';
 
 const name = 'Shin Code';
 export const siteTitle = 'Next.js blog';
@@ -31,6 +32,11 @@ function Layout({ children, home }) {
         )}
       </header>
       <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">← ホームへ戻る</Link>
+        </div>
+      )}
     </div>
   );
 }
